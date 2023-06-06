0692650dda02de80b7156ee1712cd0d60278996b
awgeboiii <135436527+awgeboiii@users.noreply.github.com>
2023-06-06 18:13:41 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-const swiper = new Swiper(".swiper", {
-    effect: "fade",
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
+const tabItem = document.querySelectorAll('.tabs_btn_item');
+const tabContent = document.querySelectorAll('.tabs_content_item');
+
+tabItem.forEach(function(element){
+  element.addEventListener('click',open)
+})
+
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach(function(item){
+    item.classList.remove('tbi-active');
+  })
+
+  tabTarget.classList.add('tbi-active');
+
+  tabContent.forEach(function(item){
+    item.classList.remove('tci-active')
   });
+
+  document.querySelector(`#${button}`).classList.add('tci-active');
+}
+
+const swiper = new Swiper(".swiper", {
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+});
+
+tbi-active
